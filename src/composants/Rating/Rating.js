8869df78eb61b1b0a6f30e032b5bdda4d5340098
nelumbo <remi.chambiet@laposte.net>
2022-06-09 11:48:57 +0200

    mise en forme header logement

--- src/composants/Rating/Rating.js
+++ src/composants/Rating/Rating.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-function rating(props) {
+function rating(Nrating) {
+    let htmlrating;
+    let starfull=<i className="fa-solid fa-star-sharp"></i>;
+    let starempty=<i className="fa-solid fa-star-sharp"></i>;
 
-  return 
+    switch(Nrating){
+        case 0:
+        htmlrating = <div className='rating'>{starempty}{starempty}{starempty}{starempty}{starempty}</div>;
+        break;
+      case 1:
+        htmlrating = <div className='rating'>{starfull}{starempty}{starempty}{starempty}{starempty}</div>;
+        break;
+      case 2:
+        htmlrating = <div className='rating'>{starfull}{starfull}{starempty}{starempty}{starempty}</div>;
+         break;
+      case 3:
+        htmlrating = <div className='rating'>{starfull}{starfull}{starfull}{starempty}{starempty}</div>;
+        break;
+      case 4:
+        htmlrating = <div className='rating'>{starfull}{starfull}{starfull}{starfull}{starempty}</div>;
+        break;
+      case 5:
+        htmlrating = <div className='rating'>{starfull}{starfull}{starfull}{starfull}{starfull}</div>;
+        break;
+        default:
+        }
+        
+
+  return htmlrating;
   }
   export default rating;
--- src/pages/Fiche_logements/Fiche_logement.js
+++ src/pages/Fiche_logements/Fiche_logement.js
@@ -3,9 +3,7 @@ import * as React from 'react';
 import { useParams } from "react-router-dom";
 import Collapse from "../../composants/Collapse/Collapse";
 import logements from "../../logements.json" ;
-import Carousel from '../../composants/Carrousel/Carrousel';
-
-
+// import rating from '../../composants/Rating/Rating';
 
 
 function Fiche_logement() {
@@ -19,23 +17,51 @@ function Fiche_logement() {
   let htmllistequi='';
   let htmlhost='';
   let htmlpicture='';
+  let htmlrat='';
+  let htmlrating;
+  let starfull=<img src='/assets/etoile_pleine.png'></img>;
+  let starempty=<img src='/assets/Vectoretoile_vide.png'></img>;
+
   // htmltag=<tagfiche name="hello"></tagfiche>;
   // htmlCarousel=<Carousel children >{logement.cover}</Carousel>;
 
   const id=useParams()
   logements.forEach((logement) => {
     if(id.id===logement.id)
-            { 
+            {   
+              const intrating=parseInt(logement.rating);
+
+              switch(intrating){
+            case 0:
+                htmlrat = <div className='rating'>{starempty}{starempty}{starempty}{starempty}{starempty}</div>;
+              break;
+            case 1:
+              htmlrat = <div className='rating'>{starfull}{starempty}{starempty}{starempty}{starempty}</div>;
+              break;
+            case 2:
+              htmlrat = <div className='rating'>{starfull}{starfull}{starempty}{starempty}{starempty}</div>;
+               break;
+            case 3:
+              htmlrat = <div className='rating'>{starfull}{starfull}{starfull}{starempty}{starempty}</div>;
+              break;
+            case 4:
+              htmlrat = <div className='rating'>{starfull}{starfull}{starfull}{starfull}{starempty}</div>;
+              break;
+            case 5:
+              htmlrat = <div className='rating'>{starfull}{starfull}{starfull}{starfull}{starfull}</div>;
+              break;
+              default:
+              }
               htmlpicture=logement.pictures.map((image)=><img src={image}></img>);
               htmltitre=<h2 className='titre'>{logement.title}</h2>;
-              htmlhost=<div className='host'><div className='hostname'>{logement.host.name}</div><img src={logement.host.picture} alt={logement.host.name}></img></div>;
+              htmlhost=<div className='host'><div className='avatar'><div className='hostname'>{logement.host.name}</div><img src={logement.host.picture} alt={logement.host.name}></img></div>{htmlrat}</div>;
               htmltag=logement.tags.map((tagname)=><h3 className='tag'>{tagname}</h3>);
               htmllocation=<p className='location'>{logement.location}</p>;
-              htmldescription=<Collapse name="description" description={logement.description} ></Collapse>;
+              htmldescription=<Collapse name="description" description={logement.description}></Collapse>;
               htmllistequi=logement.equipments.map((equipment)=><li className='equipement'>{equipment}</li>);
               htmlequipement=<Collapse name="equipements" description={htmllistequi} ></Collapse>
             }
         });
-    return <section className='fichelogement'>{htmlpicture}<section className="details"><div className='wrapperdetails'>{htmltitre}{htmllocation}<div className='wrappertag'>{htmltag}</div></div>{htmlhost}</section>{htmldescription}{htmlequipement}</section>;
+    return <section className='fichelogement'>{htmlpicture}<section className="details"><div className='wrapperdetails'><div className='headerdetails'>{htmltitre}{htmllocation}</div><div className='wrappertag'>{htmltag}</div></div>{htmlhost}</section>{htmldescription}{htmlequipement}</section>;
   }
   export default Fiche_logement;
